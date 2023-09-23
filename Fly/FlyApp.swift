2dab48dc56034ed22e5dbba98790e55be65259e0
awilliams88 <arpitwilliams@yahoo.com>
2023-09-23 11:46:47 +0530

    Initial Commit

---
+++ Fly/FlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlyApp.swift
+//  Fly
+//
+//  Created by Arpit Williams on 23/09/23.
+//
+
+import SwiftUI
+
+@main
+struct FlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
