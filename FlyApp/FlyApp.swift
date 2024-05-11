52334fc0cb59f89c01eb610566dfc924224c0be7
awilliams88 <arpitwilliams88@gmail.com>
2024-05-11 11:50:23 +0530

    Added app delegate adaptor

--- FlyApp/FlyApp.swift
+++ FlyApp/FlyApp.swift
@@ -9,9 +9,23 @@ import SwiftUI
 
 @main
 struct FlyApp: App {
+
+  @Environment(\.scenePhase) private var scenePhase
+  @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
   var body: some Scene {
     WindowGroup {
       ContentView()
+    }
+    .onChange(of: scenePhase) {
+      switch $0 {
+      case .background:
+        break
+      case .active:
+        break
+      default:
+        break
+      }
     }
   }
 }
