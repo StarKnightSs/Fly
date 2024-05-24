baa0dc5cb07abe9c0458e7b1c3d7e49da70f668a
awilliams88 <arpitwilliams88@gmail.com>
2024-05-24 18:43:07 +0530

    Added file extension to generate icon preview.

---
+++ FlyKit/Sources/FlyKit/Extensions/FileExtension.swift
@@ -0,0 +1,25 @@
+//
+// FileExtension.swift
+// Created by Arpit Williams on 24/05/24.
+// Copyright (c) 2024 StarKnights Technologies
+
+import FileServer
+import QuickLookThumbnailing
+import UIKit
+
+// MARK: - QuickLookThumbnailing
+
+public extension File {
+
+  @MainActor
+  func generatePreviewIcon() async throws -> UIImage {
+    let request = QLThumbnailGenerator.Request(
+      fileAt: url,
+      size: CGSize(width: 60, height: 60),
+      scale: UIScreen.main.scale,
+      representationTypes: .icon
+    )
+    let generator = QLThumbnailGenerator.shared
+    return try await generator.generateBestRepresentation(for: request).uiImage
+  }
+}
