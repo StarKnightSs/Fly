9a54cfcbb64a2b155c2f517e64e1aadf79b5d186
awilliams88 <arpitwilliams88@gmail.com>
2024-06-08 22:17:24 +0530

    Added utility to format seconds.

--- FlyKit/Sources/FlyKit/Utilities/Utilities.swift
+++ FlyKit/Sources/FlyKit/Utilities/Utilities.swift
@@ -16,3 +16,14 @@ func share(_ items: [Any]) {
   let window = connectedScenes.first?.windows.first { $0.isKeyWindow }
   window?.rootViewController?.present(activity, animated: true)
 }
+
+func format(_ seconds: Double) -> String {
+  var suffix = ""
+  switch seconds {
+  case 0 ..< 60: suffix = "sec"
+  case 60 ..< 3600: suffix = "min"
+  default: suffix = "hrs"
+  }
+  let time = seconds.formatted(.number.precision(.fractionLength(2)))
+  return String(format: "%@ %@", time, suffix)
+}
