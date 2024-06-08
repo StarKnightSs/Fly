285a01f2d72919b290f227684468ffcf8a697929
awilliams88 <arpitwilliams88@gmail.com>
2024-06-08 23:38:17 +0530

    Fixed error handling for upload request.

--- FlyKit/Sources/FlyServer/Controller/FileController.swift
+++ FlyKit/Sources/FlyServer/Controller/FileController.swift
@@ -53,77 +53,90 @@ struct FileController: RouteCollection {
   }
 
   func upload(_ req: Request) async throws -> Response {
+    do {
+      // Get file name & size from request
+      guard var filename = req.parameters.get("filename"),
+            let filesize = req.parameters.get("filesize")
+      else { throw Abort(.badRequest) }
 
-    // Get file name & size from request
-    guard var filename = req.parameters.get("filename"),
-          let filesize = req.parameters.get("filesize")
-    else { throw Abort(.badRequest) }
+      // Start audio playback for background processing
+      AudioManager.shared.play()
 
-    // Start audio playback for background processing
-    AudioManager.shared.play()
-
-    // Update filename & fileUrl if file already exists
-    var tempFileUrl = try filesManager.filePath(for: filename)
-    if filesManager.fileExists(at: tempFileUrl) {
-      filename = "(Copy-\(Int.random(in: 1 ..< 50))) ".appending(filename)
-      tempFileUrl = try filesManager.filePath(for: filename)
-    }
-
-    // Setup handle for file url
-    let fileUrl = tempFileUrl
-    let fileHandle = try await req.application.fileio.openFile(
-      path: fileUrl.relativePath, mode: .write,
-      flags: .allowFileCreation(), eventLoop: req.eventLoop
-    ).get()
-    defer { try? fileHandle.close() }
-
-    let stream = req.eventLoop.makePromise(of: Void.self)
-    let sequential = Sequential(future: req.eventLoop.makeSucceededFuture(()))
-
-    // Initiate progress tracking
-    if let totalSize = Int64(filesize) {
-      await ProgressManager.shared.initiate(with: totalSize)
-    }
-
-    req.body.drain {
-      switch $0 {
-      case let .buffer(buffer):
-        sequential.future = sequential.future.flatMap {
-          req.application.fileio.write(
-            fileHandle: fileHandle,
-            buffer: buffer,
-            eventLoop: req.eventLoop
-          )
-        }
-
-        // Update progress
-        Task(priority: .high) {
-          await ProgressManager.shared.updateProgress(
-            bytes: Int64(buffer.readableBytes)
-          )
-        }
-
-      case let .error(error):
-        try? FileManager.default.removeItem(at: fileUrl)
-        stream.fail(error)
-
-      case .end:
-        stream.succeed(())
+      // Update filename & fileUrl if file already exists
+      var tempFileUrl = try filesManager.filePath(for: filename)
+      if filesManager.fileExists(at: tempFileUrl) {
+        filename = "(Copy-\(Int.random(in: 1 ..< 50))) ".appending(filename)
+        tempFileUrl = try filesManager.filePath(for: filename)
       }
-      return req.eventLoop.makeSucceededVoidFuture()
+
+      // Setup file handle for file url
+      let fileUrl = tempFileUrl
+      let fileHandle = try await req.application.fileio.openFile(
+        path: fileUrl.relativePath, mode: .write,
+        flags: .allowFileCreation(), eventLoop: req.eventLoop
+      ).get()
+      defer { try? fileHandle.close() }
+
+      // Create promise & future to handle byte buffer stream
+      let stream = req.eventLoop.makePromise(of: Void.self)
+      let sequential = Sequential(future: req.eventLoop.makeSucceededFuture(()))
+
+      // Initiate progress tracking
+      if let totalSize = Int64(filesize) {
+        await ProgressManager.shared.initiate(with: totalSize)
+      }
+
+      // Collect all streaming bytes
+      req.body.drain {
+        switch $0 {
+        case let .buffer(buffer):
+          sequential.future = sequential.future.flatMap {
+            req.application.fileio.write(
+              fileHandle: fileHandle,
+              buffer: buffer,
+              eventLoop: req.eventLoop
+            )
+          }
+
+          // Update progress
+          Task(priority: .high) {
+            await ProgressManager.shared.updateProgress(
+              bytes: Int64(buffer.readableBytes)
+            )
+          }
+
+        case let .error(error):
+          try? FileManager.default.removeItem(at: fileUrl)
+          stream.fail(error)
+
+        case .end:
+          stream.succeed(())
+        }
+        return req.eventLoop.makeSucceededVoidFuture()
+      }
+
+      // Await for stream & sequential
+      try await stream.futureResult.get()
+      try await sequential.future.get()
+
+      // Call update handler
+      updateHandler?(fileUrl, .POST)
+
+      // End progress
+      AudioManager.shared.stop()
+      await ProgressManager.shared.endProgress()
+
+      // Print saved file path
+      print("Path \(fileUrl.absoluteString)")
+
+      // Redirect to home
+      return req.redirect(to: "/")
+
+    } catch {
+      AudioManager.shared.stop()
+      await ProgressManager.shared.endProgress()
+      throw (error)
     }
-
-    try await stream.futureResult.get()
-    try await sequential.future.get()
-    AudioManager.shared.stop()
-    updateHandler?(fileUrl, .POST)
-
-    // End progress
-    await ProgressManager.shared.endProgress()
-
-    print("Path \(fileUrl.absoluteString)")
-
-    return req.redirect(to: "/")
   }
 }
 
